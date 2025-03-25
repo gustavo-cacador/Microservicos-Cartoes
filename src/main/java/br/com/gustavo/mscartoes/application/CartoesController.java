f83bc423cfbe1e6f9482377f4250c85ef86119f7
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-03-25 02:09:52 -0300

    Implementando o microservice de Cartões pt1

--- src/main/java/br/com/gustavo/mscartoes/application/CartoesController.java
+++ src/main/java/br/com/gustavo/mscartoes/application/CartoesController.java
@@ -1,15 +1,27 @@
 package br.com.gustavo.mscartoes.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.gustavo.mscartoes.dto.CartaoSaveRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("cartoes")
+@RequiredArgsConstructor
 public class CartoesController {
+
+    private final CartaoService service;
 
     @GetMapping
     public String status() {
         return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity cadastra(@RequestBody CartaoSaveRequest request) {
+        var cartao = request.toModel();
+        service.save(cartao);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
