c1f767f1778254e00b33fd70060ea378f1e0a3a2
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-03-30 18:46:31 -0300

    refactor: espaçamento

--- src/main/java/br/com/gustavo/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
+++ src/main/java/br/com/gustavo/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -23,14 +23,17 @@ public class EmissaoCartaoSubscriber {
     public void receberSolicitacaoEmissao(@Payload String payload) {
         try {
             var mapper = new ObjectMapper();
+
             DadosSolicitacaoEmissaoCartao dados = mapper.readValue(payload, DadosSolicitacaoEmissaoCartao.class);
             Cartao cartao = cartaoRepository.findById(dados.getIdCartao()).orElseThrow();
             ClienteCartao clienteCartao = new ClienteCartao();
+
             clienteCartao.setCartao(cartao);
             clienteCartao.setCpf(dados.getCpf());
             clienteCartao.setLimite(dados.getLimiteLiberado());
 
             clienteCartaoRepository.save(clienteCartao);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
